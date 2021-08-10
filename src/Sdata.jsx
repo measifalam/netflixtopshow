f0a02ebacee5ee85e4450421a0a60b03ab60d5f7
Md Asif Alam <54733354+measifalam@users.noreply.github.com>
2021-08-10 18:38:07 +0530

    Create Sdata.jsx

---
+++ src/Sdata.jsx
@@ -0,0 +1,39 @@
+const Sdata = [
+  {
+    key:1,
+    sname: "How I meet you your Mother",
+    imgsrc: "https://ih1.redbubble.net/image.217953011.3603/poster,504x498,f8f8f8-pad,600x600,f8f8f8.jpg",
+    title: "A Netflix Original Series",
+    link: "https://www.amazon.com/How-Met-Your-Mother-Season/dp/B000WDSZ8E",
+  },
+  {
+    key:2,
+    sname: "I Am Not Okay With This",
+    imgsrc: "https://ih1.redbubble.net/image.545864571.5252/poster,504x498,f8f8f8-pad,600x600,f8f8f8.u1.jpg",
+    title: "A Netflix Original Series",
+    link: "https://www.netflix.com/in/title/80175405",
+  },
+  {
+    key:3,
+    sname: "YOU",
+    imgsrc: "https://ih1.redbubble.net/image.1089535231.7480/poster,504x498,f8f8f8-pad,600x600,f8f8f8.jpg",
+    title: "A Netflix Original Series",
+    link: "https://www.netflix.com/in/title/80244781",
+  },
+  {
+    key:4,
+    sname: "Dynasty",
+    imgsrc: "https://ih1.redbubble.net/image.513512325.4322/poster,504x498,f8f8f8-pad,600x600,f8f8f8.u8.jpg",
+    title: "A Netflix Original Series",
+    link: "https://www.netflix.com/in/title/80175722"
+},
+  {
+    key:5,
+    sname: "The Rain",
+    imgsrc: "https://ih1.redbubble.net/image.1184883621.2419/poster,504x498,f8f8f8-pad,600x600,f8f8f8.jpg",
+    title: "A Netflix Original Series",
+    link: "https://www.netflix.com/in/title/80236318",
+  },
+];
+
+export default Sdata;
